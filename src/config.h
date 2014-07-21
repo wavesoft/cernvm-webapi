e72d29133069606e0d9920eb7526e7a07b308ba2
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2014-07-21 11:32:29 +0200

    Switched to 2.0.5

--- src/config.h
+++ src/config.h
@@ -23,7 +23,7 @@
 #define CONFIG_H
 
 // The CernVM WebAPI Version
-#define CERNVM_WEBAPI_VERSION "2.0.4"
+#define CERNVM_WEBAPI_VERSION "2.0.5"
 
 // The port where the cernvm webapi is running
 #define CERNVM_WEBAPI_PORT 	  5624
--- src/platform/unix/main.cpp
+++ src/platform/unix/main.cpp
@@ -20,6 +20,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <dlfcn.h>
 #include <cernvm/CrashReport.h>
 
 // Webserver
