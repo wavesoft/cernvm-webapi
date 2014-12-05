0d252bd72e43ccbca12ab649b5d0252fc2a0b965
wavesoft <wavesoft@github.com>
2014-12-05 17:37:11 +0100

    Adding version information

--- installer/ui/index2.php
+++ installer/ui/index2.php
@@ -1,9 +1,22 @@
 <?php
 
+$AGENT_DEFAULT_ARCH = "i386";
+$AGENT_ARCHMAP = array(
+	"wow64" => "amd64",
+	""
+);
+
+$PLATFORM_MAP = array(
+	'windows' => array(
+			'ext' => '.exe',
+			'name' => 'Windows'
+		)
+);
+
 /**
  * Architecture mapping
  */
-$ARCH_MAP = array(
+$PKG_ARCH_MAP = array(
     'i486'      => 'i386',
     'i586'      => 'i386',
     'i686'      => 'i386',
@@ -20,104 +33,13 @@ $ARCH_MAP = array(
 /**
  * Platform mapping depending on extension
  */
-$EXT_MAP = array(
+$PKG_EXT_MAP = array(
     'exe'       => 'windows',
     'pkg'       => 'macintosh',
     'deb'       => 'linux',
     'rpm'       => 'linux'
 );
 
-/**
- * Platform tree
- */
-$PLATFORM_DICT = array(
-    'windows' => array(
-        'default' => array(
-            'icon' => 'img/plaf-windows.png',
-            'title' => 'Windows',
-            'flavor' => 'XP SP3+'
-            )
-        ),
-    'macintosh' => array(
-        'default' => array(
-            'icon' => 'img/plaf-macintosh.png',
-            'title' => 'Mac OS X',
-            'flavor' => '10.8+'
-            )
-        ),
-    'linux' => array(
-
-        'lucid' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '10.04 LTS (Lucid Lynx)'
-            ),
-        'maverick' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '10.10 (Maverick Meerkat)'
-            ),
-        'natty' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '11.04 (Natty Narwhal)'
-            ),
-        'oneiric' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '11.10 (Oneiric Ocelot)'
-            ),
-        'precise' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '12.04 LTS (Precise Pangolin)'
-            ),
-        'quantal' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '12.10 (Quantal Quetzal)'
-            ),
-        'raring' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '13.04 (Raring Ringtail)'
-            ),
-        'saucy' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '13.10 (Saucy Salamander)'
-            ),
-        'trusty' => array(
-            'icon' => 'img/plaf-linux-ubuntu.png',
-            'title' => 'Ubuntu',
-            'flavor' => '14.04 LTS (Trusty Tahr)'
-            ),
-
-        'squeeze' => array(
-            'icon' => 'img/plaf-linux-debian.png',
-            'title' => 'Debian',
-            'flavor' => '6.0 (Squeeze)'
-            ),
-        'wheezy' => array(
-            'icon' => 'img/plaf-linux-debian.png',
-            'title' => 'Debian',
-            'flavor' => '7.0 (Wheezy)'
-            ),
-        'jessie' => array(
-            'icon' => 'img/plaf-linux-debian.png',
-            'title' => 'Debian',
-            'flavor' => '8.0 (Jessie)'
-            ),
-
-        'slc6' => array(
-            'icon' => 'img/plaf-linux-slc.png',
-            'title' => 'Scientific Linux CERN',
-            'flavor' => '6.0'
-            ),
-
-        
-        )
-);
 
 /**
  * Parses a user agent string into its important parts
@@ -264,39 +186,39 @@ function parse_user_agent( $u_agent = null ) {
  * Detect user-agent
  */
 $valid_platforms = array( "android","cros","iphone","ipad","linux","macintosh","windows" );
-$USER = parse_user_agent();
-$PLATFORM = "";
-if (isset($USER['platform'])) {
-    $PLATFORM = strtolower($USER['platform']);
+$AGENT = parse_user_agent();
+$AGENT_PLATFORM = "";
+if (isset($AGENT['platform'])) {
+    $AGENT_PLATFORM = strtolower($AGENT['platform']);
 }
 if (isset($_GET['plaf'])) {
     $plaf = strtolower($_GET['plaf']);
     if (in_array($plaf, $valid_platforms)) {
-        $PLATFORM = $plaf;
+        $AGENT_PLATFORM = $plaf;
     }
 }
 
 /**
- * Detect platform flavor
+ * Detect user agent architecture 
  */
-$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
-echo $ua;
-$FLAVOR = "default";
-$PLATFORM_FLAVORS = $PLATFORM_DICT[$PLATFORM];
-if ((count($PLATFORM_FLAVORS) > 1) || !isset($PLATFORM_FLAVORS['default'])) {
-    // Scan user agent to look for that flavor
-    foreach ($PLATFORM_FLAVORS as $flav => $info) {
-        if (stristr($ua, $flav)) {
-            $FLAVOR = $flav;
-            break;
-        }
-    }
+$AGENT_ARCH = $AGENT_DEFAULT_ARCH;
+foreach ($AGENT_ARCHMAP as $k => $arch) {
+	if (stristr($_SERVER['HTTP_USER_AGENT'], $k)) {
+		$AGENT_ARCH = $arch;
+		break;
+	}
 }
 
 /**
- * Scan packages and build lookup table
+ * Prepare the possible options
+ * 
+ * Expected package syntax:
+ * cvmwebapi-2.0.10.pkg
+ *
+ * <package name>_<architecture>-<version>[.<flavor>].<ext>
+ *
  */
-$PACKAGES = array();
+$ALL_OPTIONS = array();
 $pkgs = scandir("packages");
 foreach ($pkgs as $f => $v) {
     if (substr($v,0,1) == '.')
@@ -311,8 +233,8 @@ foreach ($pkgs as $f => $v) {
     $i_arch = strtolower($parts[0]);
 
     // Fix architecture if exists on arch_map
-    if (!isset($ARCH_MAP[$i_arch])) continue;
-    $i_arch = $ARCH_MAP[$i_arch];
+    if (!isset($PKG_ARCH_MAP[$i_arch])) continue;
+    $i_arch = $PKG_ARCH_MAP[$i_arch];
 
     // Extract extension
     if (count($parts) < 2) continue;
@@ -320,8 +242,8 @@ foreach ($pkgs as $f => $v) {
     $i_ext = array_pop($parts);
 
     // Map platform according to extension
-    if (!isset($EXT_MAP[$i_ext])) continue;
-    $i_platf = $EXT_MAP[$i_ext];
+    if (!isset($PKG_EXT_MAP[$i_ext])) continue;
+    $i_platf = $PKG_EXT_MAP[$i_ext];
 
     // Extract flavor (if exists)
     $i_flavor = "default";
@@ -358,17 +280,16 @@ foreach ($pkgs as $f => $v) {
     if (count($parts) != 0) continue;
 
     // Store on packages
-    if (!isset($PACKAGES[$i_platf])) $PACKAGES[$i_platf]=array();
-    if (!isset($PACKAGES[$i_platf][$i_flavor])) $PACKAGES[$i_platf][$i_flavor]=array();
-    $PACKAGES[$i_platf][$i_flavor][] = array(
+    $ALL_OPTIONS[] = array(
         'version'   => $s_ver,
         'intver'    => $i_ver,
         'arch'      => $i_arch,
         'size'      => $s_size,
+        'flavor'	=> $i_flavor,
+        'platf'		=> $i_platf,
         'name'      => $v,
         'href'      => 'packages/'.$v
     );
-
 }
 
 /**
@@ -378,142 +299,26 @@ function cmp_intver($b, $a) {
     if ($a['intver'] == $b['intver']) return 0;
     return ($a['intver'] < $b['intver']) ? -1 : 1;
 }
-foreach ($PACKAGES as $os => $flavors) {
-    foreach ($flavors as $i => $packages) {
-        usort($PACKAGES[$os][$i], 'cmp_intver');
-    }
-}
-
-include("pages/cflavor.php");
-
-//print_r($PACKAGES);
-die();
+usort($ALL_OPTIONS, 'cmp_intver');
 
 /**
- * Detect linux variants
+ * Scan user information and prepare the options
  */
-$PLATFORM_NAME = ucfirst($PLATFORM);
-$PLATFORM_ARCH = "x32";
-if ($PLATFORM == 'linux') {
-    $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
-    if (strstr($ua, "ubuntu")) {
-        $PLATFORM .= "-deb";
-        $PLATFORM_NAME = "Ubuntu Linux";
-    } else if (strstr($ua, "debian")) {
-        $PLATFORM .= "-deb";
-        $PLATFORM_NAME = "Debian Linux";
-    } else if (strstr($ua, "red hat")) {
-        $PLATFORM .= "-rpm";
-        $PLATFORM_NAME = "RedHat Linux";
-    } else if (strstr($ua, "suse") || strstr($ua, "rhel") || strstr($ua, "fedora") || strstr($ua, "slc")) {
-        $PLATFORM .= "-rpm";
-        $PLATFORM_NAME = "Linux (RedHat variant)";
-    } else if (strstr($ua, "gentoo") || strstr($ua, "mint")) {
-        $PLATFORM .= "-tgz";
-        $PLATFORM_NAME = "Linux (Generic)";
-    } else {
-        $PLATFORM .= "-rpm";
-        $PLATFORM_NAME = "Linux (RedHat variant)";
-    }
-    if (strstr($ua, "x86_64") || strstr($ua, "amd64")) {
-        $PLATFORM_ARCH = "x64";
-        $PLATFORM_NAME .= " (64-bit)";
-    }
-    $PLATFORM .= "-" . $PLATFORM_ARCH;
+$OPTIONS = array();
+foreach ($ALL_OPTIONS as $o) {
+
+	// Filter platform
+	if (($AGENT_PLATFORM != "") && ($AGENT_PLATFORM != $o['platf'])) continue;
+
+	// Filter architecture
+	if ($AGENT_ARCH != $o['arch']) continue;
+
+	// Store on options
+	$OPTIONS[] = $o;
+
 }
 
-/**
- * List files in the packages directory
- */
-$pkgs = scandir("packages");
-$PACKAGES = array(
-        'macintosh' => array(),
-        'windows' => array(),
-        'linux-deb-x32' => array(),
-        'linux-deb-x64' => array(),
-        'linux-rpm-x32' => array(),
-        'linux-rpm-x64' => array(),
-        'linux-gz-x32' => array(),
-        'linux-gz-x64' => array()
-    );
-foreach ($pkgs as $f => $v) {
-    if (substr($v,0,1) == '.')
-        continue;
 
-    // Extract file parts
-    $ext = substr($v,-4);
-    $parts = explode("-", substr($v,0,-4));
-    $verParts = explode(".", $parts[1]);
-    $intVer = ((int)($verParts[0])) * 10000 +
-              ((int)($verParts[1])) * 100 +
-              ((int)($verParts[2]));
-
-    if ($ext == ".pkg") {
-        $PACKAGES['macintosh'][] = array(
-                'version' => $parts[1],
-                'intver' => $intVer,
-                'name' => $v,
-                'href' => 'packages/'.$v
-            );
-    } else if ($ext == ".msi") {
-        $PACKAGES['windows'][] = array(
-                'version' => $parts[1],
-                'intver' => $intVer,
-                'name' => $v,
-                'href' => 'packages/'.$v
-            );
-    } else if ($ext == ".deb") { 
-        $f = array(
-            'version' => $parts[1],
-            'intver' => $intVer,
-            'name' => $v,
-            'href' => 'packages/'.$v
-        );
-        if (strstr($v, "amd64") || strstr($v, "x86_64")) {
-            $PACKAGES['linux-deb-x64'][] = $f;
-        } else {
-            $PACKAGES['linux-deb-x32'][] = $f;
-        }
-    } else if ($ext == ".rpm") { 
-        $f = array(
-            'version' => $parts[1],
-            'intver' => $intVer,
-            'name' => $v,
-            'href' => 'packages/'.$v
-        );
-        if (strstr($v, "amd64") || strstr($v, "x86_64")) {
-            $PACKAGES['linux-rpm-x64'][] = $f;
-        } else {
-            $PACKAGES['linux-rpm-x32'][] = $f;
-        }
-    } else if ($ext == ".tgz") {
-        $f = array(
-            'version' => $parts[1],
-            'intver' => $intVer,
-            'name' => $v,
-            'href' => 'packages/'.$v
-        );
-        if (strstr($v, "amd64") || strstr($v, "x86_64")) {
-            $PACKAGES['linux-tgz-x64'][] = $f;
-        } else {
-            $PACKAGES['linux-tgz-x32'][] = $f;
-        }
-    }
-}
-
-/**
- * Sort the platform versions
- */
-$PLATFORM_PACKAGES = $PACKAGES[$PLATFORM];
-usort($PLATFORM_PACKAGES, 'cmp_intver');
-
-/**
- * Pick installer page, depending on the platform
- */
-if (!$PLATFORM || empty($PLATFORM_PACKAGES)) {
-    include("pages/missing.php");
-} else {
-    include("pages/install.php");
-}
+print_r($OPTIONS);
 
 ?>
