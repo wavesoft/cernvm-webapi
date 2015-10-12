180256f6cbd75240a5df04c3cd0dcd591ae39e99
wavesoft <wavesoft@github.com>
2015-10-12 16:25:03 +0200

    Adding build script for vs2013

---
+++ prepare-vs2013-vt120_xp.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set CONFIG=Release
+if %1%a==a goto begin_config
+set CONFIG=%1
+if %CONFIG%==Release goto begin_config
+if %CONFIG%==Debug goto begin_config
+echo Configuration can either be 'Debug' or 'Release'!
+goto end
+
+:begin_config
+set BUILDDIR=build_vs2013_t120_xp-%CONFIG%
+if not exist "%BUILDDIR%" mkdir "%BUILDDIR%"
+cd "%BUILDDIR%"
+cmake -DCMAKE_BUILD_TYPE=%CONFIG% -DCRASH_REPORTING=OFF -DTARGET_ARCH="i386" -G"Visual Studio 12" -T"v120_xp" ..
+
+:end
